74db53a870e82a66ae163a3bd3644faac694553f
NazwaDafaRamadhan <nazwadafaramadhan@gmail.com>
2023-05-15 11:56:46 +0700

    ok

---
+++ src/views/LandingPageView.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary navbar-custom">
+        <a href="#"><img src="public/assets/img/WhatsApp_Image_2023-05-05_at_15.27.46-removebg-preview.png" alt="SentUs">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ms-auto">
+                
+                <a class="nav-link" href="#">Layanan</a>
+                <a class="nav-link" href="#">Contact Us</a>
+                <a class="nav-link" href="#">Login</a>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <section></section>
+    </div>
+</template>
